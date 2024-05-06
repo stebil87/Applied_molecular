5c2970e41ae2eb4864142c95583840863c1e75e7
stebil87 <Stebill87@gmail.com>
2024-05-06 09:24:58 +0200

    xai updated

--- utils/rfcv.py
+++ utils/rfcv.py
@@ -1,14 +1,21 @@
 from sklearn.feature_selection import RFECV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
-def apply_RFECV_and_add(df):
+def perform_RF_feature_selection(df):
     X = df.drop(['BBB', 'smiles'], axis=1)
     y = df['BBB']
-    model = RandomForestClassifier()
-    cv = StratifiedKFold(5)
-    rfe = RFECV(estimator=model, step=100, cv=cv, scoring='accuracy')
-    X_rfe = rfe.fit_transform(X, y)
-    selected_features = X.columns[rfe.support_]
-    sel_df = df[selected_features.tolist() + ['BBB', 'smiles']]
-    return sel_df
+    rf = RandomForestClassifier(n_estimators=100, random_state=42)
+    rf.fit(X, y)
+    feature_importances = rf.feature_importances_
+    feature_importance_df = pd.DataFrame({'Feature': X.columns, 'Importance': feature_importances})
+    feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='Importance', y='Feature', data=feature_importance_df.head(10), palette='viridis')
+    plt.title('Top 10 Important Features')
+    plt.xlabel('Importance')
+    plt.ylabel('Feature')
+    plt.show()
